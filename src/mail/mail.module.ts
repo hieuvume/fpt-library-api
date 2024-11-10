b6ad67818bb94a47ea6ea78f767bcfb971f45c44
Hieu Vu <hieuvu.me@gmail.com>
2024-11-10 09:57:51 +0700

    Added mail processor

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,39 +1,48 @@
-import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Global, Module } from '@nestjs/common';
-import { MailService } from './mail.service';
-import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
+import { MailerModule } from "@nestjs-modules/mailer";
+import { HandlebarsAdapter } from "@nestjs-modules/mailer/dist/adapters/handlebars.adapter";
+import { Global, Module } from "@nestjs/common";
+import { MailService } from "./mail.service";
+import { join } from "path";
+import { ConfigService } from "@nestjs/config";
+import { BullModule } from "@nestjs/bull";
+import { MailProcessor } from "./mail.processor";
 
 @Global()
 @Module({
-    imports: [
-        MailerModule.forRootAsync({
-            useFactory: async (config: ConfigService) => ({
-                transport: {
-                    host: config.get('MAIL_HOST'),
-                    port: 465,
-                    secure: true,
-                    auth: {
-                        user: config.get('MAIL_USER'),
-                        pass: config.get('MAIL_PASSWORD'),
-                    },
-                },
-                defaults: {
-                    from: `"No Reply" <${config.get('MAIL_FROM')}>`,
-                },
-                template: {
-                    dir: join(__dirname, 'templates'),
-                    adapter: new HandlebarsAdapter(),
-                    options: {
-                        strict: true,
-                    },
-                },
-            }),
-            inject: [ConfigService],
-        }),
-    ],
-    providers: [MailService],
-    exports: [MailService],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get("MAIL_HOST"),
+          port: 465,
+          secure: true,
+          auth: {
+            user: config.get("MAIL_USER"),
+            pass: config.get("MAIL_PASSWORD"),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${config.get("MAIL_FROM")}>`,
+        },
+        template: {
+          dir: join(__dirname, "templates"),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    BullModule.registerQueue({
+      name: "mail-queue",
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
+  providers: [MailService, MailProcessor],
+  exports: [MailService],
 })
-export class MailModule { }
+export class MailModule {}
